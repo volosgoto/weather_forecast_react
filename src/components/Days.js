70be5b32cbb2d2a8e0b9cde735b13be84b322f59
andrey <volosgoto@yahoo.com>
2019-04-21 16:00:47 +0300

    add list of weather by city

--- src/components/Days.js
+++ src/components/Days.js
@@ -15,7 +15,6 @@ class Days extends Component {
 
   render() {
     if (this.props.location.city) {
-      console.log("About", this.props.location);
       let { name } = this.props.location.city;
       let { country } = this.props.location.city;
       let { humidity } = this.props.location.list[0].main;
@@ -26,48 +25,93 @@ class Days extends Component {
       let { description } = this.props.location.list[0].weather[0];
       let { wind } = this.props.location.list[0];
 
-      // function date(dt_txt) {}
+
+      // let list = this.props.location.cities[0];
+      // let weatherListbyCity = list;
+      console.log('Days', this.props.location.cities);
       return (
+        <div>
+          <div className="row mt-3">
+            <div className="col-md-8 m-auto text-center">
+              <h4 className="card-title">Days Forecast</h4>
+              <div className="row">
+                <div className="col">
+                  <h2>
+                    {name}, {country}
+                  </h2>
+                  <p>Current weather {dt_txt}</p>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col">
+                  <img
+                    src={`http://openweathermap.org/img/w/${icon}.png`}
+                    alt="weather_icon"
+                  />
+                  <div className="col">{description}</div>
+                </div>
+              </div>
+              <div className="row ">
+                <div className="col mt-3 mb-2">
+                  <p>{tempConverter(temp)} &#8451;</p>
+                </div>
+                <div className="col mt-3 mb-2">Wind {wind.speed} m/sec</div>
+                <div className="col mt-3 mb-2">Humidity {humidity} %</div>
+                <div className="col mt-3 mb-2">
+                  Pressure {pressureConverter(pressure)} mmHg
+              </div>
+              </div>
 
-        <div className="row mt-3">
-          <h1>Days Forecast</h1>
-          <div className="col-md-8 m-auto text-center">
-            <h4 className="card-title">Weather today</h4>
-            <div className="row">
-              <div className="col">
-                <h2>
-                  {name}, {country}
-                </h2>
-                <p>Current weather {dt_txt}</p>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col">
-                <img
-                  src={`http://openweathermap.org/img/w/${icon}.png`}
-                  alt="weather_icon"
-                />
-                <div className="col">{description}</div>
-              </div>
-            </div>
-            <div className="row ">
-              <div className="col mt-3 mb-2">
-                <p>{tempConverter(temp)} &#8451;</p>
-              </div>
-              <div className="col mt-3 mb-2">Wind {wind.speed} m/sec</div>
-              <div className="col mt-3 mb-2">Humidity {humidity} %</div>
-              <div className="col mt-3 mb-2">
-                Pressure {pressureConverter(pressure)} mmHg
-              </div>
-            </div>
-
-            <div className="col mt-5">
-              <Link to="/" className="btn btn-info">
-                <i className="fa fa-home" /> Back Home
+              <div className="col mt-5">
+                <Link to="/" className="btn btn-info">
+                  <i className="fa fa-home" /> Back Home
               </Link>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="container">
+              <div className="row mt-3 justify-content-center">
+                {/* {cities.map(city => {
+
+                  return (
+                    <div>
+                      <div className="col" key={city.city.name}>
+                        <div className="row">
+                          <div className="col">
+                            <h2>{city.city.name}</h2>
+                            <img
+                              src={`http://openweathermap.org/img/w/${
+                                city.list[0].weather[0].icon
+                                }.png`}
+                              alt="weather_icon"
+                            />
+                          </div>
+                        </div>
+                        <div className="row">
+                          <div className="col">
+                            {city.list[0].weather[0].description}
+                          </div>
+                        </div>
+
+                        <div className="row">
+                          <div className="col mt-2">
+                            <h3>
+                              {tempConverter(city.list[0].main.temp)} &#8451;
+                          </h3>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })} */}
+              </div>
             </div>
           </div>
         </div>
+
+
+
       );
     } else {
       return <Spinner />;

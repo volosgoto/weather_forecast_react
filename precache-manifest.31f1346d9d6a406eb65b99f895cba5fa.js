b600575fad75ef2e18638c2768904a9284536b9c
volosgoto <volosgoto@yahoo.com>
2019-04-23 16:15:05 +0300

    Updates

--- precache-manifest.31f1346d9d6a406eb65b99f895cba5fa.js
+++ precache-manifest.31f1346d9d6a406eb65b99f895cba5fa.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/weather_forecast_react/static/js/runtime~main.6b477b9a.js"
   },
   {
-    "revision": "1e36b7e6ddec76c317bf",
-    "url": "/weather_forecast_react/static/js/main.a16f5aa8.chunk.js"
+    "revision": "676c84474e6bb3ae08e5",
+    "url": "/weather_forecast_react/static/js/main.32af4bc8.chunk.js"
   },
   {
     "revision": "57d2a26e1a7c4d3256e0",
     "url": "/weather_forecast_react/static/js/2.c6b1dc95.chunk.js"
   },
   {
-    "revision": "1e36b7e6ddec76c317bf",
+    "revision": "676c84474e6bb3ae08e5",
     "url": "/weather_forecast_react/static/css/main.e9c62afe.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/weather_forecast_react/static/css/2.0e603913.chunk.css"
   },
   {
-    "revision": "eb336f2acb6d30e0c87d9eaea7c81ff4",
+    "revision": "8b55cf6452dd6731607b4f45142447de",
     "url": "/weather_forecast_react/index.html"
   }
 ];

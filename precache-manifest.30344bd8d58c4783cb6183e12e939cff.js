067859b99b82cbdbf78b3a13a09f55a4ccdd67c0
volosgoto <volosgoto@yahoo.com>
2019-04-23 15:24:41 +0300

    Updates

---
+++ precache-manifest.30344bd8d58c4783cb6183e12e939cff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "78a664720c2c173e1964",
+    "url": "/weather_forecast_react/static/js/runtime~main.6b477b9a.js"
+  },
+  {
+    "revision": "bb801a25214fc07f2ded",
+    "url": "/weather_forecast_react/static/js/main.a8021e4e.chunk.js"
+  },
+  {
+    "revision": "9ff3b0542808f2e05f0a",
+    "url": "/weather_forecast_react/static/js/2.3600836a.chunk.js"
+  },
+  {
+    "revision": "bb801a25214fc07f2ded",
+    "url": "/weather_forecast_react/static/css/main.b7d06b1c.chunk.css"
+  },
+  {
+    "revision": "9ff3b0542808f2e05f0a",
+    "url": "/weather_forecast_react/static/css/2.0eed1841.chunk.css"
+  },
+  {
+    "revision": "2c5ddbf2f41b97937c1646f0de29c66f",
+    "url": "/weather_forecast_react/index.html"
+  }
+];

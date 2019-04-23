04dd1d26a88cc3d839e82db8c921d52853fce667
volosgoto <volosgoto@yahoo.com>
2019-04-23 15:28:50 +0300

    Updates

---
+++ precache-manifest.3530faba9516d4e47d05f0e526754144.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5a8d1ad9a44e99e2c051",
+    "url": "/static/js/main.656071c3.chunk.js"
+  },
+  {
+    "revision": "9ff3b0542808f2e05f0a",
+    "url": "/static/js/2.3600836a.chunk.js"
+  },
+  {
+    "revision": "5a8d1ad9a44e99e2c051",
+    "url": "/static/css/main.b7d06b1c.chunk.css"
+  },
+  {
+    "revision": "9ff3b0542808f2e05f0a",
+    "url": "/static/css/2.0eed1841.chunk.css"
+  },
+  {
+    "revision": "25828c396a2dc1d6593996a2f628122b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/weather_forecast_react/precache-manifest.30344bd8d58c4783cb6183e12e939cff.js"
+  "/precache-manifest.3530faba9516d4e47d05f0e526754144.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/weather_forecast_react/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
